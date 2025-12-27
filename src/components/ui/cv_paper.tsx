11f5a0809a42977a8f88bb49298962d0b6692e53
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-12-27 16:52:35 +0700

    feat: add CV paper UI component with sub-elements for layout.

--- src/components/ui/cv_paper.tsx
+++ src/components/ui/cv_paper.tsx
@@ -2,63 +2,55 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 const Paper = React.forwardRef<
-    HTMLDivElement,
-    React.AllHTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.AllHTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-    <div
-        ref={ref}
-        className={cn('shadow-2xl w-auto h-370', className)}
-        {...props}
-    />
+  <div
+    ref={ref}
+    className={cn('h-370 w-auto shadow-2xl', className)}
+    {...props}
+  />
 ))
 Paper.displayName = 'Paper'
 
 const PaperHeader = React.forwardRef<
-    HTMLDivElement,
-    React.AllHTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.AllHTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-    <div
-        ref={ref}
-        className={cn('flex items-center space-x-15 px-20 pt-15 pb-5', className)}
-        {...props}
-    />
+  <div
+    ref={ref}
+    className={cn('flex items-center space-x-15 px-20 pt-15 pb-5', className)}
+    {...props}
+  />
 ))
 PaperHeader.displayName = 'PaperHeader'
 
 const ProfileImg = React.forwardRef<
-    HTMLDivElement, 
-    React.AllHTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.AllHTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-    <div
-        ref={ref}
-        className={cn('flex-shrink-0', className)}
-        {...props}
-    />
+  <div ref={ref} className={cn('flex-shrink-0', className)} {...props} />
 ))
 ProfileImg.displayName = 'ProfileImg'
 
 const VerticalLine = React.forwardRef<
-    HTMLDivElement, 
-    React.AllHTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.AllHTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-    <div
-        ref={ref}
-        className={cn('w-0.5 h-70 bg-black', className)}
-        {...props}
-    />
+  <div ref={ref} className={cn('h-70 w-0.5 bg-black', className)} {...props} />
 ))
 VerticalLine.displayName = 'VerticalLine'
 
 const HorizontalLine = React.forwardRef<
-    HTMLDivElement, 
-    React.AllHTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.AllHTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-    <div
-        ref={ref}
-        className={cn('h-0.5 bg-black mx-auto w-225', className)}
-        {...props}
-    />
+  <div
+    ref={ref}
+    className={cn('mx-auto h-0.5 w-225 bg-black', className)}
+    {...props}
+  />
 ))
 HorizontalLine.displayName = 'HorizontalLine'
 
-export {Paper, ProfileImg, PaperHeader, VerticalLine, HorizontalLine}
+export { Paper, ProfileImg, PaperHeader, VerticalLine, HorizontalLine }
